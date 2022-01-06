ad9e0cb8226a198509f4a230eac64b4f48868253
Nikolai Vazov <n.a.vazov@usit.uio.no>
2022-01-06 17:31:27 +0100

    First commit for root squash Galaxy installation using proxy via socks5

---
+++ files/galaxy/oidc_files/lifeportal_fox.py
@@ -0,0 +1,23 @@
+"""
+Backend for OpenID Connect Fox UiO
+"""
+
+from social_core.backends.open_id_connect import OpenIdConnectAuth
+
+
+class LifeportalFoxOpenIdConnect(OpenIdConnectAuth):
+    name = 'lifeportalfox'
+    OIDC_ENDPOINT = 'https://research.educloud.no/login'
+    EXTRA_DATA = [
+        ('expires_in', 'expires_in', True),
+        ('refresh_token', 'refresh_token', True),
+        ('id_token', 'id_token', True),
+        ('other_tokens', 'other_tokens', True),
+    ]
+
+    DEFAULT_SCOPE = ['openid', 'email']
+    JWT_DECODE_OPTIONS = {'verify_at_hash': False}
+
+    def get_user_details(self, response):
+        username_key = self.setting('USERNAME_KEY', default=self.USERNAME_KEY)
+        return {'username': response.get(username_key), 'email': response.get('email')}
