9ec4d4654484dcf59813f0c8242e1ef5d4a6c4df
Nikolai Vazov <n.a.vazov@usit.uio.no>
2022-05-09 16:04:06 +0200

    Changed logo (favicon -> fox icon) and edited Login.vue for customized login page

--- files/galaxy/web_files/Login.vue
+++ files/galaxy/web_files/Login.vue
@@ -5,71 +5,39 @@
                 <div class="col col-lg-6">
                     <b-alert :show="messageShow" :variant="messageVariant" v-html="messageText" />
                     <b-form id="login" @submit.prevent="submitGalaxyLogin()">
-                        <b-card no-body header="Welcome to Lifeportal-Galaxy on the FOX cluster at UiO, please log in by clicking on the FOX icon below">
+                        <b-card no-body header="Welcome to Lifeportal-Galaxy on the FOX cluster at UiO">
                             <b-card-body>
                                 <div>
                                     <!-- standard internal galaxy login -->
-                                    <b-form-group label="Public Name or Email Address">
-                                        <b-form-input name="login" type="text" v-model="login" />
+                                    <b-form-group label="How to log in">
+											<b-form-text>
+												To log into Lifeportal read this block and then click on the FOX icon below.
+											</b-form-text>
                                     </b-form-group>
-                                    <b-form-group label="Password">
-                                        <b-form-input name="password" type="password" v-model="password" />
-                                        <b-form-text>
-                                            Forgot password?
-                                            <a @click="reset" href="javascript:void(0)" role="button"
-                                                >Click here to reset your password.</a
-                                            >
-                                        </b-form-text>
+                                     <b-form-group label="Two-factor authentication (2FA)">
+											<b-form-text>
+												The Lifeportal service uses two-factor authentication. Please install Google, Microsoft or another authenticator on your smartphone
+											</b-form-text>
                                     </b-form-group>
-                                    <b-button name="login" type="submit">Login</b-button>
+									<b-form-group label="Educloud project required">
+											<b-form-text>
+												To use Lifeportal, please apply first for an educloud project
+												</p>
+												<a href="https://research.educloud.no/login"><img src="https://research.educloud.no/_next/static/images/logo-text-ee0d4c59633474b318e00573994c7e60.svg" width="100"  height="20"></a>
+												</p>
+												 and scan the QR-code for 2FA
+											</b-form-text>
+                                    </b-form-group>
                                 </div>
                                 <div v-if="enable_oidc">
                                     <!-- OIDC login-->
                                     <external-login :login_page="true" />
                                 </div>
                             </b-card-body>
-                            <b-card-footer>
-                                Don't have an account?
-                                <span v-if="allowUserCreation">
-                                    <a
-                                        id="register-toggle"
-                                        href="javascript:void(0)"
-                                        role="button"
-                                        @click.prevent="toggleLogin"
-                                        >Register here.</a
-                                    >
-                                </span>
-                                <span v-else>
-                                    Registration for this Galaxy instance is disabled. Please contact an administrator
-                                    for assistance.
-                                </span>
-                            </b-card-footer>
                         </b-card>
                     </b-form>
-
-                    <b-modal centered id="duplicateEmail" ref="duplicateEmail" title="Duplicate Email" ok-only>
-                        <p>
-                            There already exists a user with this email. To associate this external login, you must
-                            first be logged in as that existing account.
-                        </p>
-
-                        <p>
-                            Reminder: Registration and usage of multiple accounts is tracked and such accounts are
-                            subject to termination and data deletion. Connect existing account now to avoid possible
-                            loss of data.
-                        </p>
-                        -->
-                    </b-modal>
                 </div>
             </template>
-
-            <template v-else>
-                <confirmation @setRedirect="setRedirect" />
-            </template>
-
-            <div v-if="show_welcome_with_login" class="col">
-                <b-embed type="iframe" :src="welcome_url" aspect="1by1" />
-            </div>
         </div>
     </div>
 </template>
